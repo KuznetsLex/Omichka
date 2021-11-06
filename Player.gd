dd20424aaeb361afddeed11e252d83ab258cde5b
KuznetsLex <59352807+KuznetsLex@users.noreply.github.com>
2021-11-06 17:59:38 +0600

    Add files via upload
    Initialize Grand Father and Design Second Scene

--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var speed : int = 500
+var speed : int = 400
 var jumpforce : int = 1000
 var gravity : int = 2500
 
@@ -10,16 +10,18 @@ var vel = Vector2()
 
 func _physics_process(delta):
 	vel.y += gravity * delta
-
+	if position.x>1280:
+		get_tree().change_scene("res://Main.tscn")
 	
 	if Input.is_action_pressed("ui_left"):
 		vel.x =-speed
-		$player.flip_h = false
+		$AnimatedSprite.flip_h = false
 	elif Input.is_action_pressed("ui_right"):
 		vel.x =speed
-		$player.flip_h = true
+		$AnimatedSprite.flip_h = true
 	else:
 		vel.x=0
+		$AnimatedSprite.play("idle")
 		
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		vel.y -= jumpforce
